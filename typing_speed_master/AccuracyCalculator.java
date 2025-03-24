a821868f796eebe36f3a20b33cabfa1a84016238
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-03-24 20:21:48 +0530

    added accuracy calculator

---
+++ typing_speed_master/AccuracyCalculator.java
@@ -0,0 +1,18 @@
+package typing_speed_master;
+
+public class AccuracyCalculator {
+
+    // Method to calculate the accuracy of the user's typing
+    public static double calculateAccuracy(String[] displayedWords, String[] typedWords) {
+        int correctWords = 0;
+        int totalWords = displayedWords.length;
+
+        for (int i = 0; i < totalWords; i++) {
+            if (i < typedWords.length && displayedWords[i].equals(typedWords[i])) {
+                correctWords++;
+            }
+        }
+
+        return (double) correctWords / totalWords * 100;
+    }
+}
